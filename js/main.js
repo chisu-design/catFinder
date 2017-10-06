a10379d28a87813c90654834b0f940bff1800a82
Chi Su <chisu@Chis-MacBook-Pro.local>
2017-10-06 00:03:51 -0400

    add more firebase stuff and fixed some btn problems

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
-	$("#uploadBtn").hide();
+	// Start Firebase
+    var config = {
+        apiKey: "AIzaSyCCvts9xarbQN5yK7DLenVrGwZuTo2sVWg",
+        authDomain: "catfinder-b90a1.firebaseapp.com",
+        databaseURL: "https://catfinder-b90a1.firebaseio.com/",
+        projectId: "catfinder-b90a1",
+        storageBucket: "gs://catfinder-b90a1.appspot.com",
+        messagingSenderId: "286010632123"
+    };
+    // library
+    firebase.initializeApp(config);
 
-	$(".file").on("change"), function(event){
-		$("#uploadBtn").show();
-	}
+    //prepare database
+    var database = firebase.database();
+
+
+    $(function() {
 	
 });
