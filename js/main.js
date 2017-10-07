1a1fe886b39ab286d8535c2b5e95bb924ba86fa1
Chi Su <chisu@Chis-MacBook-Pro.local>
2017-10-07 00:06:13 -0400

    did stuff again

--- js/main.js
+++ js/main.js
@@ -14,7 +14,92 @@ $(document).ready(function() {
     //prepare database
     var database = firebase.database();
 
+        $(function() {
 
-    $(function() {
+        database.ref('cat/').on("value", function(snapshot) {
+            var results = snapshotToArray(snapshot.val());
+            console.log(results);
+        }, function(error) {
+            console.log("Error: " + error.code);
+        });
+
+        function snapshotToArray(snapshot) {
+            var returnArr = [];
+
+            for (shot in snapshot) {
+                returnArr.push(snapshot[shot]);
+            }
+
+
+            return returnArr;
+        };
+
+        //form data
+        var location = null;
+        var files = null;
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                location = { lat: position.coords.latitude, lng: position.coords.longitude };
+            });
+        } else {
+            //execute code here if browser don't support geolocation
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+
+        $('#files').change(function(event) {
+            files = event.target.files;
+
+        });
+
+
+        $('#myForm').submit(function(event) {
+            event.preventDefault();
+
+            if (files.length > 0) {
+                var possible = 'abcedfghijklmnopqrstuvwxyzABCEDFGHIJKLMNOPQRSTUVWXYZ0123456789',
+                    numOfChar = 20,
+                    uuid = '',
+                    images = [];
+
+                for (c = 0; c < numOfChar; c++) {
+                    uuid += possible.charAt(Math.floor(Math.random() * possible.length));
+
+                }
+
+                for (i = 0, numOfFiles = files.length; i < numOfFiles; i++) {
+
+                    var imagePath = 'images/' + uuid + '/' + files[i].name;
+                    var storageRef = firebase.storage().ref(imagePath);
+
+                    var insertImage = function(snapshot){
+
+                        images.push(snapshot.metadata.downloadURLs[0]);
+
+                        //insert data into firebase
+                        if (images.length === numOfFiles) {
+                            database.ref('cat').push({
+                            	id: uuid,
+                                name: event.target.catName.value,
+                                location: location,
+                                images: images
+                            });
+                        }
+                    	
+                    }
+
+                    storageRef.put(files[i]).then(insertImage);
+
+                }
+            }
+
+
+
+
+        });
+
+
+
+    });
 	
 });
